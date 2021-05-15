d65d7fa273c00a0c1b8a801abd137b8a485745b1
nguyenquochoa <nguyenquochoa2000tb2@gmail.com>
2021-05-15 17:01:04 +0700

    add feature book in server

---
+++ QLTV_V2/QLTV_V2/BLL/BookBLL.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QLTV_V2.DAL;
+using QLTV_V2.Data;
+using QLTV_V2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QLTV_V2.BLL
+{
+    public class BookBLL
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly BookDAL _bookDAL;
+
+        public BookBLL(ApplicationDbContext context)
+        {
+            _context = context;
+            _bookDAL = new BookDAL(_context);
+        }
+
+        public IEnumerable<Object> GetAll()
+        {
+            try
+            {
+                return _bookDAL.GetAll();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error from BookBLL: " + ex.Message.ToString());
+            }
+        }
+
+        public ActionResult<Object> GetById(int id)
+        {
+            try
+            {
+                return _bookDAL.GetById(id);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error from BookBLL: " + ex.Message.ToString());
+            }
+        }
+
+        public void AddBook(Book book)
+        {
+            try
+            {
+                if (checkBookCategoryExist(book.BookCategory_Id))
+                {
+                    _bookDAL.AddBook(book);
+                }
+                else
+                {
+                    throw new Exception("Book Category doesn't exist");
+                }
+                  
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error from BookBLL: " + ex.Message.ToString());
+            }
+        }
+
+        public void EditBook(int id, Book newBook)
+        {
+            try
+            {
+                
+                Book oldBook = _context.Book.Where(us => us.Id == id).SingleOrDefault();
+                if (checkBookCategoryExist(newBook.BookCategory_Id) && oldBook != null)
+                {
+                    _bookDAL.EditBook(oldBook, newBook);
+                }
+                else
+                {
+                    throw new Exception("Book Category or Book doesn't exist");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error from BookBLL: " + ex.Message.ToString());
+            }
+        }
+
+        public void DeleteBook(int id)
+        {
+            try
+            {
+                Book book = _context.Book.Find(id);
+                if (book != null)
+                {
+                    _bookDAL.DeleteBook(book);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error from BookBLL: " + ex.Message.ToString());
+            }
+        }
+
+        public bool checkBookCategoryExist(int? bookCategory_id)
+        {
+            return _context.BookCategory.Find(bookCategory_id) != null ? true : false;
+        } 
+            
+    }
+}

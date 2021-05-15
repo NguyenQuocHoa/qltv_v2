c679d292e931643e17e11cb658602be4c8b8da7d
nguyenquochoa <nguyenquochoa2000tb2@gmail.com>
2021-05-15 16:20:50 +0700

    add feature Book category in server

---
+++ QLTV_V2/QLTV_V2/BLL/BookCategoryBLL.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QLTV_V2.DAL;
+using QLTV_V2.Data;
+using QLTV_V2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QLTV_V2.BLL
+{
+    public class BookCategoryBLL
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly BookCategoryDAL _bookCategoryDAL;
+
+        public BookCategoryBLL(ApplicationDbContext context)
+        {
+            _context = context;
+            _bookCategoryDAL = new BookCategoryDAL(_context);
+        }
+
+        public IEnumerable<Object> GetAll()
+        {
+            try
+            {
+                return _bookCategoryDAL.GetAll();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error from BookCategoryBLL");
+            }
+        }
+
+        public ActionResult<Object> GetById(int id)
+        {
+            try
+            {
+                return _bookCategoryDAL.GetById(id);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error from BookCategoryBLL");
+            }
+        }
+
+        public void AddBookCategory(BookCategory bookCategory)
+        {
+            try
+            {
+                _bookCategoryDAL.AddBookCategory(bookCategory);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error from BookCategoryBLL");
+            }
+        }
+
+        public void EditBookCategory(int id, BookCategory newBookCategory)
+        {
+            try
+            {
+                BookCategory oldBookCategory = _context.BookCategory.Where(us => us.Id == id).SingleOrDefault();
+                _bookCategoryDAL.EditBookCategory(oldBookCategory, newBookCategory);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error from BookCategoryBLL");
+            }
+        }
+
+        public void DeleteBookCategory(int id)
+        {
+            try
+            {
+                BookCategory bookCategory = _context.BookCategory.Find(id);
+                if (bookCategory != null)
+                {
+                    _bookCategoryDAL.DeleteBookCategory(bookCategory);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error from BookCategoryBLL");
+            }
+        }
+    }
+}
--- QLTV_V2/QLTV_V2/Controllers/BookCategoryController.cs
+++ QLTV_V2/QLTV_V2/Controllers/BookCategoryController.cs
@@ -1,10 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using QLTV_V2.BLL;
+using QLTV_V2.Data;
+using QLTV_V2.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace QLTV_V2.Controllers
 {
@@ -12,36 +13,84 @@ namespace QLTV_V2.Controllers
     [ApiController]
     public class BookCategoryController : ControllerBase
     {
-        // GET: api/<ValuesController>
+        private readonly BookCategoryBLL _bookCategoryBLL;
+        public BookCategoryController(ApplicationDbContext context)
+        {
+            _bookCategoryBLL = new BookCategoryBLL(context);
+        }
+
+
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IEnumerable<Object> Get()
         {
-            return new string[] { "value1", "value2" };
+            try
+            {
+                return _bookCategoryBLL.GetAll();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            return null;
         }
 
-        // GET api/<ValuesController>/5
+
         [HttpGet("{id}")]
-        public string Get(int id)
+        public ActionResult<Object> Get(int id)
         {
-            return "value";
+            try
+            {
+                return _bookCategoryBLL.GetById(id);
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "Server Error");
+            }
         }
 
-        // POST api/<ValuesController>
+
         [HttpPost]
-        public void Post([FromBody] string value)
+        public IActionResult Post([FromBody] BookCategory bookCategory)
         {
-        }
+            try
+            {
+                _bookCategoryBLL.AddBookCategory(bookCategory);
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "Server Error");
+            }
+            return Ok(bookCategory);
+        } 
 
-        // PUT api/<ValuesController>/5
+
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public IActionResult Put(int id, [FromBody] BookCategory bookCategory)
         {
+            try
+            {
+                _bookCategoryBLL.EditBookCategory(id, bookCategory);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Server Error");
+            }
+            return Ok(id);
         }
 
-        // DELETE api/<ValuesController>/5
+
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public IActionResult Delete(int id)
         {
+            try
+            {
+                _bookCategoryBLL.DeleteBookCategory(id);
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "Server Error");
+            }
+            return Ok(id);
         }
     }
 }
